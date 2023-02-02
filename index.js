4649f53dedd6b665b24ce4b1bd5f45354c5b5715
Leiladany <leiladany16@gmail.com>
2023-02-02 11:54:28 +0000

    viking lab done

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 //Make sure to require your model so you can connect to the DB
 const { Model } = require("mongoose");
+const VikingModel = require("./models/User");
 
 //This line of code connects to the Database
 require("./db");
@@ -7,3 +8,61 @@ require("./db");
 //Iteration 2:
 
 //Iteration 3:
+
+const NewViking = {
+    firstName: "Bjørn",
+    familyName: "Evensen",
+    strenght: 100,
+    weapon: "String",
+};
+
+const NewVikingArray = [
+    {firstName: "Frode",
+    familyName: "Bengtsson",
+    strenght: 70,
+    weapon: "Axe",
+},
+{firstName: "Halfdan",
+    familyName: "Ellingboe",
+    strenght: 30,
+    weapon: "Broom",
+},
+{firstName: "Ødger",
+    familyName: "Gunvaldsen",
+    strenght: 200,
+    weapon: "Mjölnir",
+}
+]
+
+VikingModel.findByIdAndUpdate('63dba2408b29bb08a12cac2c', {firstName:"Odin", strenght: 1000}, {new: true})
+.then ((updatedViking) => {
+    console.log("updatedViking", updatedViking);
+})
+.catch((err) => {
+    console.log("err", err);
+})
+
+/* VikingModel.insertMany(NewVikingArray)
+.then((newVikingNDB) => {
+    console.log("newVikingNDB", newVikingNDB);
+})
+.catch((err) => {
+    console.log(err)
+}) */
+
+/* VikingModel.create(NewViking)
+.then((newVikingNDB) => {
+    console.log("New Viking Added", newVikingNDB);
+})
+.catch((err) => {
+    console.log("Error adding newViking", err);
+}) */
+
+VikingModel.findByIdAndDelete ('63db9c9b27ecf785ccfa8f44')
+.then((deletedViking) => {
+    console.log("Deleted Viking", deletedViking);
+})
+.catch((err) => {
+    console.log("Error deleting Viking", err);
+})
+
--- models/User.js
+++ models/User.js
@@ -3,3 +3,22 @@ const { Schema, model } = require("mongoose");
 //Create a viking schema here
 //<======Don't forget Module.export ============>
 //<================Schema=====================>
+
+const vikingSchema = new Schema({
+    firstName: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    familyName:{
+        type: String,
+        require: true,
+        unique: true,
+    },
+    strenght: Number, 
+
+    weapon: String,
+})
+
+const VikingModel = model ("Viking", vikingSchema)
+module.exports = VikingModel;
